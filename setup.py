2abd046ece7b83cbd1bbaa0b9bf4b908ce9fac89
Daniel Shapiro <dcshapiro@gmail.com>
2021-10-21 07:37:20 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 from setuptools import setup, find_packages
 setup(
     name='smallCompanyType',
-    version="2.0",
+    version="3.0",
     packages=['smallCompanyType','smallCompanyType.models'],
     package_data={'': ['*.h5','*.pkl',]},
     include_package_data=True,
-    install_requires=["Keras>=2.1.2","sklearn","numpy>=1.13.3","h5py>=2.7.1"],
+    install_requires=["Keras>=2.1.2","transformers","numpy>=1.13.3","h5py>=2.7.1"],
 )
